8dd416e9d9a8551c2eb527c9aed777ad92919a4b
贺博 <hebo@chaoxing.com>
2017-09-14 17:17:23 +0800

    去掉ExpressBrute拦截

--- API/app.js
+++ API/app.js
@@ -37,7 +37,7 @@ function random(size) {
 }
 
 
-app.post('/compile', bruteforce.prevent, function (req, res) {
+app.post('/compile', function (req, res) {
 
 	var language = req.body.language;
 	var code = req.body.code;
